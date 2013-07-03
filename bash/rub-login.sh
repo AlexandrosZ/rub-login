44b9c13b2362baf4294e03d60eb13efe44927989
AlexandrosZ <alexandros.ellada@gmail.com>
2013-07-03 23:30:01 +0200

    Removed bad code from bash/rub-login.sh

--- bash/rub-login.sh
+++ bash/rub-login.sh
@@ -3,17 +3,12 @@
 username=YourUsername #Replace "YourUsername" with your username
 password=YourPassword #Replace "YourPassword" with your password
 
+wget -q --post-data 'loginid='"$username"'&password='"$password"'&action=Login' https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin -O tmplogin
 
-ping -c 1 "8.8.8.8" > /dev/null
-if [ "$?" -eq 0 ] ; then
-  echo "Already logged in"
+reply=$(grep "Authentisierung gelungen" tmplogin)
+if [ "$reply" ]; then
+	echo "Logged in succesfully"
 else
-	wget -q --post-data 'loginid='"$username"'&password='"$password"'&action=Login' https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin -O tmplogin
-
-	reply=$(grep "Authentisierung gelungen" tmplogin)
-	if [ "$reply" ]; then
-		echo "Logged in succesfully"
-	else
-		echo "Login failed, check your username/password"
-	fi
+	echo "Login failed, check your username/password"
 fi
+
